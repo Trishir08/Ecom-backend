317a89d84ae851fcda2ebcace56697c538ca91d9
Trishir087 <trishir@sportjacks.com>
2024-09-21 01:53:30 +0530

    changes

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,5 @@
 import { Product } from "../models/productSchema.js"
+import { User } from "../models/userschema.js";
 
 const addProducts = async (req , res)=>{
 
@@ -62,9 +63,50 @@ const getAllProducts = async(req , res)=>{
 }
 
 const getNewCollections = async(req , res)=>{
-    let products = await Product.find() ; 
+    let products = await Product.find({}) ; 
     let newcollection = products.slice(1).slice(-8) ; 
     res.send(newcollection) ; 
 }
 
-export { getNewCollections , getAllProducts ,addProducts , removeProducts} ;
+const popularWomen = async(req,res)=>{
+    let products = await Product.find({category : "women"}) ;
+    let popular_in_women = products.slice(0,4) ; 
+    res.send(popular_in_women) ; 
+}
+
+const fetchUser = async(req ,res , next)=>{
+try{
+    const token = req.header('auth-token') ;
+    if(!token){
+        res.status(401).send({error : "Please authenticate urself"})
+    }else{
+        const data = jwt.verify(token , 'secret_ecom') ;
+        req.user = data.user ; 
+        next() ; 
+    }
+}catch(error){
+    res.status(401).send({errors : "please auhenticATE URSEL"}) ;
+}
+}
+
+const addToCart = async(req , res)=>{
+    let userData = await User.findOne({_id : req.user.id}) ;
+    userData.cartData[req.body.itemId] += 1 ;
+    await User.findOneAndUpdate({_id : req.user.id} , {cartData : userData.cartData})
+    res.send("Added successfully") ; 
+}
+
+const removeFromCart = async(req , res)=>{
+    let userData = await User.findOne({_id : req.user.id}) ;
+    if( userData.cartData[req.body.itemId] > 0)
+    userData.cartData[req.body.itemId] -= 1 ;
+    await User.findOneAndUpdate({_id : req.user.id} , {cartData : userData.cartData})
+    res.send("Added successfully") ; 
+}
+
+const getCart = async(req , res)=>{
+    let userData = await User.findOne({_id : req.user.id}) ; 
+    res.json(userData.cartData) ;
+}
+
+export {getCart , getNewCollections , getAllProducts ,addProducts , removeProducts , popularWomen , addToCart , fetchUser , removeFromCart} ;
--- routers/product.route.js
+++ routers/product.route.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import express from "express"
-import { addProducts, getAllProducts, getNewCollections, removeProducts } from "../controllers/productController.js";
+import { addProducts, getAllProducts, getNewCollections, removeProducts , popularWomen , addToCart , fetchUser , removeFromCart , getCart} from "../controllers/productController.js";
 
 const router = Router() ; 
 
@@ -8,5 +8,10 @@ router.route("/addproduct").post(addProducts) ;
 router.route("/removeproduct").post(removeProducts) ;
 router.route("/allProducts").get(getAllProducts) ;
 router.route("/newcollections").get(getNewCollections) ;
+router.route('/popular-women').get(popularWomen) ;
+router.post('/addtocart' , fetchUser , addToCart) ; 
+router.post('/removeFromCart' , fetchUser , removeFromCart) ; 
+router.post('/getCart' , fetchUser , getCart) ; 
+
 
 export default router ; 
